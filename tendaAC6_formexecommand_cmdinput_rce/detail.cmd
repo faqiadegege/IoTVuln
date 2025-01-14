3ae72a80548e8caea81795a269bd6efc0fa0621b
faqiadegege <15650765179@163.com>
2025-01-14 15:33:35 +0800

    Update detail.cmd

--- tendaAC6_formexecommand_cmdinput_rce/detail.cmd
+++ tendaAC6_formexecommand_cmdinput_rce/detail.cmd
@@ -1,1 +1,26 @@
+# Tenda AC6 : 15.03.05.16_multi
 
+There is a overflow vulnerability in AC6 : 15.03.05.16_multi. In the formexeCommand function(target url: exeCommand), the parameters cmdinput by http will cause remote command execution.
+
+
+![]()
+
+
+
+
+![]()
+
+
+
+
+![]()
+
+
+
+
+![]()
+
+
+
+
+![]()
